542ae4199990f0d3fe1f006df101b931a1635b7b
Navedijaz480 <mn.navedijaz480@gmail.com>
2023-12-18 22:39:23 +0500

    final

--- index.js
+++ index.js
@@ -1,39 +1,30 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-require("dotenv").config();
-const cors = require("cors");
-
-const userRouter = require("./routes/userRoutes");
-
 const app = express();
-
-app.use(cors());
-app.use(bodyParser.json({ limit: "10mb" }));
-
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
-
-const conn = mongoose.connection;
-conn.on("error", console.error.bind(console, "MongoDB connection error:"));
-conn.once("open", function () {
-  console.log("Connected to MongoDB...");
-});
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 app.use(express.json());
-app.use("/user", userRouter);
 
-app.all("*", (req, res) => {
-  res.status(404).send("<h1>404! Page not found</h1>");
+
+const userRoutes = require("./routes/userRoutes");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+// console.log("Database url", process.env.DATABASE_URL);
+mongoose
+  .connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+
+  .then(() => app.listen("4000", () => console.log(`server running on: 4000`)))
+
+  .catch((error) => console.log("Error Message", error.message));
+const conn = mongoose.connection;
+conn.on("open", function () {
+  console.log("connected...");
 });
 
-// Check if running in AWS Lambda environment
-if (process.env.AWS_LAMBDA_FUNCTION_NAME) {
-  // Export the Express app for use in AWS Lambda
-  module.exports = app;
-} else {
-  // Start a local server if not in AWS Lambda environment
-  const PORT = process.env.PORT || 4000;
-  app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-  });
-}
+
+
+app.use("/api/user", userRoutes);
