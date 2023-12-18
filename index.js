11eee155ebc0e7fab205f5c416e43a859c061935
Navedijaz480 <mn.navedijaz480@gmail.com>
2023-12-18 22:18:35 +0500

    store data

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
 const express = require("express");
-const app = express();
-const cors = require("cors");
-const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+var bodyParser = require("body-parser");
 require("dotenv").config();
+var cors = require("cors");
 
-app.use(express.json());
+const userRouter = require("./routes/userRoutes");
 
 
-const userRoutes = require("./routes/userRoutes");
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+const app = express();
+const PORT = process.env.PORT || 4000;
+
 app.use(cors());
+app.use(bodyParser.json({ limit: "10mb" }));
 
-// console.log("Database url", process.env.DATABASE_URL);
-mongoose
-  .connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+// console.log("Database url", URL);
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 
-  .then(() => app.listen("4000", () => console.log(`server running on: 4000`)))
-
-  .catch((error) => console.log("Error Message", error.message));
 const conn = mongoose.connection;
 conn.on("open", function () {
   console.log("connected...");
 });
 
+app.use(express.json());
+app.use("/user", userRouter);
 
 
-app.use("/api/user", userRoutes);
+app.all("*", (req, res) => {
+  res.status(404).send("<h1>404! Page not found</h1>");
+});
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
